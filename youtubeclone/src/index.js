d1a46ec53ec1b4cf03420331259eee0ce0ad1d89
Byul Kim <caxbelle@gmail.com>
2022-12-21 01:48:23 -0800

    Structure

---
+++ youtubeclone/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+const app = express();
+
+// Middlewares
+const logger = morgan("dev");
+app.use(logger);
+
+// Routers
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
+
+// sending is request, server is listening the request, when web is showing you something is response
